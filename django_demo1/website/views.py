37585b0032195dbb9fda71a64c2eb8d187aea733
Amit Raj <amit.raj@dell.com>
2023-11-21 16:01:26 +0530

    first commit

---
+++ django_demo1/website/views.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+from meetings.models import Meeting
+
+def welcome(request):
+    return render(request, "website/welcome.html", {"num_meetings": Meeting.objects.count()})
+
+def date(request):
+    return HttpResponse('This page was served at '+ str(datetime.now()))
+
+def about(request):
+    return HttpResponse('This is the about page')
+
